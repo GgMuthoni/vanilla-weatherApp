12ef1690e4989aada4469331a5ee2680d351c6fe
Gillian <mbuguagillian7@gmail.com>
2022-11-18 15:46:08 +0300

    Updated current temperature and date

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
+function CurrentDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
 function showTemperature(response) {
-  console.log(response);
   document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#date").innerHTML = CurrentDate(
+    response.data.dt * 1000
+  );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -13,6 +38,7 @@ function showTemperature(response) {
     response.data.wind.speed
   );
 }
+let city = "New york";
 let apiKey = "34f6f9ebebdb251a42cde086a06a5eee";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kenya&units=metric&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 axios.get(apiUrl).then(showTemperature);
